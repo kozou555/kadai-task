29388138326430ae69a49c8fd7fcb994776db9ab
kazuya.haga <hg.future.160@icloud.com>
2021-09-04 01:36:46 +0000

    five

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_09_02_123503) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
-    t.string "conteny"
+    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
